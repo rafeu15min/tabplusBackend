4cfb46b59a9cfb6a951ebc825dd393aeb632fea0
Rafael <rafaeldepaulo7@hotmail.com>
2024-04-12 16:27:23 -0300

    atualizações de dependências

--- src/main/java/com/api/tabplusBackend/TabplusBackendApplication.java
+++ src/main/java/com/api/tabplusBackend/TabplusBackendApplication.java
@@ -2,8 +2,9 @@ package com.api.tabplusBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class TabplusBackendApplication {
 
 	public static void main(String[] args) {
